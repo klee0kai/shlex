3f119f4805b2613a0865f93445555d1fc9dbf10a
klee0kai <klee0kai@gmail.com>
2023-04-01 10:21:37 +0400

    jvm 8 toolchain

--- shlex/build.gradle.kts
+++ shlex/build.gradle.kts
@@ -6,12 +6,16 @@ plugins {
 
 group = "com.github.klee0kai.shlex"
 
-
-dependencies {
-    testImplementation("org.junit.jupiter:junit-jupiter-api:5.9.2")
-    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.9.2")
+kotlin {
+    jvmToolchain(8)
 }
 
 tasks.getByName<Test>("test") {
     useJUnitPlatform()
+}
+
+
+dependencies {
+    testImplementation("org.junit.jupiter:junit-jupiter-api:5.9.2")
+    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.9.2")
 }
